c2ef5a24d6a546ae35360f7c78d515d737aecbe7
LendaDeAfonselio <afonsof367@gmail.com>
2020-09-14 17:43:34 +0100

    delete alarm implemented and workin'

---
+++ Commands/deletealarm.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'deletealarm',
+    description: 'Deletes the alarm with a given id',
+    usage: '<prefix>deletealarm <id>',
+    execute(msg, args, client, cron, cron_list) {
+        var alarm_to_delete = args[0];
+        if (cron_list[alarm_to_delete] !== undefined) {
+            delete cron_list[alarm_to_delete];
+            msg.channel.send(`Sucessfully deleted alarm: ${alarm_to_delete}.\n`);
+        }
+        else {
+            console.log(`Impossible to delete alarm with id ${alarm_to_delete}`);
+            var myalarms_command_stg = "`myalarms`";
+            msg.channel.send(`Impossible to delete alarm with id ${alarm_to_delete}.\nPlease check if you entered the id of the alarm correctly!\nTo see your alarms, do: ${myalarms_command_stg}`);
+        }
+    }
+};
