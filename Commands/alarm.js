8ba428f124eaadbfa9b5b67ea83371386cb5d0a2
LendaDeAfonselio <48067609+LendaDeAfonselio@users.noreply.github.com>
2020-09-14 17:44:01 +0100

    Delete alarm (#2)
    * move alarmid var
    
    * delete alarm implemented and workin'

--- Commands/alarm.js
+++ Commands/alarm.js
@@ -4,8 +4,6 @@ module.exports = {
     description: 'Sets up an alarm that will be repeated',
     usage: '<prefix>alarm <m> <h> <weekday> <month> <year> <message> <target>',
     execute(msg, args, client, cron, cron_list) {
-        console.log(global_id);
-        var this_alarm_id = global_id;
         global_id += 1;
         var crono = args.slice(0, 5).join(' ');
         var message_stg = args.slice(5, args.length - 1).join(' ');
@@ -18,9 +16,9 @@ module.exports = {
                 scheduled: true
             });
             scheduledMessage.start();
-
+            let this_alarm_id = global_id;
             let alarm_user = msg.author.id;
-            let alarm_id = `${global_id}_${alarm_user}`;
+            let alarm_id = `${this_alarm_id}_${alarm_user}`;
             cron_list[alarm_id] = scheduledMessage;
         } catch (err) {
             console.err(err);
