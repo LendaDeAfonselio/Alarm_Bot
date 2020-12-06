3907826b72f82a2fdcd57ed8003191859d4044f0
LendaDeAfonselio <afonsof367@gmail.com>
2020-12-06 16:42:54 +0000

    Begin custom code for alarm :)

--- Commands/alarm.js
+++ Commands/alarm.js
@@ -1,16 +1,64 @@
 const Alarm_model = require('../models/alarm_model');
 const auth = require('./../auth.json');
 
+function small_time_interval(mins) {
+    if (mins === '*') {
+        return true;
+    }
+    if (mins.contains('-')) {
+        return true;
+    }
+    if (!mins.contains('*/')) {
+        return false;
+    }
+    let num_minutes = mins.replace('*/', '');
+    let n = parseInt(num_minutes);
+
+    return isNaN(n) || n < 15;
+}
+
+function validate_alarm_parameters(msg, cron_params, message_stg) {
+    if (cron_params.length < 6) {
+        msg.channel.send('Not enough parameters were passed, try `#alarmHelp` for more information!');
+        return false;
+    }
+    let mins = cron_params[0];
+    if (small_time_interval(mins)) {
+        msg.channel.send("The minute parameter you sent is either invalid or too short. Only time intervals bigger than 15 minutes are allowed to avoid spam");
+    }
+    let hours = cron_params[1];
+    let num = parseInt(hours);
+    if(isNaN(num) || num < 0 || num > 23){
+        msg.channel.send("The hour parameter is invalid! Try `#alarmHelp` for more information!")
+    }
+    let month_day = cron_params[2];
+    let month_num_day = parseInt(month_day);
+    if(isNaN)
+    let month = cron_params[3];
+    let weekday = cron_params[3];
+
+    let message = message_stg;
+    if (message.length > 350) { // message is too long
+        msg.channel.send('Not enough parameters were passed, try `#alarmHelp` for more information!');
+        return false;
+    }
+}
+
 module.exports = {
     name: 'alarm',
     description: 'Sets up an alarm that will be repeated\n' +
         'This alarm will send a message to the _channel_ of the _server_ in which it is activated.\n'
         + 'The parameter <target> is optional.',
-    usage: auth.prefix + 'alarm <m> <h> <month> <year> <weekday> <message> <target>',
+    usage: auth.prefix + 'alarm <m> <h> <month> <weekday> <message> <target>',
     async execute(msg, args, client, cron, cron_list, mongoose) {
         var crono = args.slice(0, 5).join(' ');
         var message_stg = args.slice(5, args.length - 1).join(' ');
         var target = args[args.length - 1];
+
+        // Check if the message has a single word and no target
+        if (target.contains('<@&')) {
+            message_stg = target;
+        }
         var guild = msg.guild.id;
 
         try {
