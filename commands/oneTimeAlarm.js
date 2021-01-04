fd18f83de983b680f3a9b0ed7dcee3bfbce31387
LendaDeAfonselio <48067609+LendaDeAfonselio@users.noreply.github.com>
2021-01-04 20:00:55 +0000

    Integrating timezones in oneTimeAlarm (#41)
    * initial skeleton of timezone integration
    
    * tweaking error messages and undefined parameter bug

--- commands/oneTimeAlarm.js
+++ commands/oneTimeAlarm.js
@@ -1,6 +1,7 @@
 const auth = require('./../auth.json');
 const utils = require('../Utils/utility_functions');
 const logging = require('../Utils/logging');
+const time_utils = require('../Utils/time_validation');
 
 function isValidDate(d) {
     return d instanceof Date && !isNaN(d);
@@ -29,10 +30,10 @@ function parseDateAndTime(date_args, hour_min_args, msg) {
     return undefined;
 }
 
-function setupCronForOTAlarm(d, msg, cron_list, now, ota, data_stg) {
+function setupCronForOTAlarm(d, msg, cron_list, now, ota, data_stg, timezone) {
 
     ota.start();
-    msg.channel.send(`Alarm for ${data_stg} has been setup`);
+    msg.channel.send(`Alarm for ${data_stg} (${timezone}) has been setup`);
     let alarm_user = msg.author.id;
     let this_alarm_id = Math.random().toString(36).substring(4);
     let alarm_id = `${auth.one_time_prefix}_${this_alarm_id}_${alarm_user}`;
@@ -59,104 +60,126 @@ module.exports = {
         + 'For a private alarm use the -p flag as the second argument otherwise it will send the message to the channel you typed the command at\n'
         + 'If no Date is specified then it will default to today\n'
         + 'P.S - These alarms are not persistent - they are not saved on a DB, therefore if the bot goes down you will have to set them up again.',
-    usage: auth.prefix + 'oneTimeAlarm <-p> <HH:MM> <Day/Month/Year> <Message>\n',
+    usage: auth.prefix + 'oneTimeAlarm <-p> <Timezone> <HH:MM> <Day/Month/Year> <Message>\n',
     async execute(msg, args, client, cron, cron_list, mongoose) {
         if (utils.hasAlarmRole(msg, auth.alarm_role_name) || utils.isAdministrator(msg)) {
-            if (args.length < 2) {
-                msg.channel.send('Insuficient arguments were passed for this alarm!\n'
-                    + 'Try $help for more information!');
-            }
-            var isPrivate = args[0].toLowerCase() === '-p';
-            var hour_min_args = args[1];
-            var date_args = '';
-            var message = '';
-            if (!isPrivate) {
-                hour_min_args = args[0];
-                if (args[1].includes('/') && args[1].length >= 3 && args[1].length <= 10) {
-                    date_args = args[1];
-                    message = args.slice(2, args.length).join(' ');
-
-                    var d = parseDateAndTime(date_args, hour_min_args, msg);
-
-                    if (isValidDate(d)) {
-                        var params_stg = date_args.toString() + ' ' + hour_min_args.toString();
-                        var now = new Date();
-                        if (d > now) {
-                            let ota = new cron(d, () => {
-                                msg.channel.send(`${message}`);
-                            });
-                            setupCronForOTAlarm(d, msg, cron_list, now, ota, params_stg);
+            if (args.length > 1) {
+                let isPrivate = args[0].toLowerCase() === '-p';
+                if (isPrivate && args.length < 4 || !isPrivate && args.length < 3) {
+                    msg.channel.send('Insuficient arguments were passed for this alarm!\n'
+                        + 'Usage: `' + auth.prefix + 'oneTimeAlarm <-p> <Timezone> <HH:MM> <Day/Month/Year> <Message>`\n'
+                        + 'Try `$help` for more information!');
+                }
+                else {
+                    let hour_min_args = args[2];
+                    let date_args = '';
+                    let message = '';
+                    if (!isPrivate) {
+                        let timezone = args[0];
+                        let difference = time_utils.get_offset_difference(timezone);
+                        if (difference === undefined) {
+                            msg.channel.send('The timezone you have entered is invalid. Please visit https://www.timeanddate.com/time/map/ for information about your timezone!')
                         } else {
-                            msg.channel.send(`The date you entered: **${params_stg}** already happened, please verify the parameters and try again!`);
+                            hour_min_args = args[1];
+                            if (args[2].includes('/') && args[2].length >= 3 && args[2].length <= 10) {
+                                date_args = args[2];
+                                message = args.slice(3, args.length).join(' ');
+
+                                var nonTransformedDate = parseDateAndTime(date_args, hour_min_args, msg);
+                                let d = time_utils.generateDateGivenOffset(nonTransformedDate, difference);
+                                if (isValidDate(d)) {
+                                    var params_stg = date_args.toString() + ' ' + hour_min_args.toString();
+                                    var now = new Date();
+                                    if (d > now) {
+                                        let ota = new cron(d, () => {
+                                            msg.channel.send(`${message}`);
+                                        });
+                                        setupCronForOTAlarm(d, msg, cron_list, now, ota, params_stg, timezone);
+                                    } else {
+                                        msg.channel.send(`The date you entered: **${params_stg}** already happened, please verify the parameters and try again!`);
+                                    }
+                                } else {
+                                    msg.channel.send(`The date _${date_args}_ that you have provided is invalid, it should be <Day/Month/Year>! Please correct any errors and try again!`);
+                                }
+                            } else {
+                                message = args.slice(2, args.length).join(' ');
+
+                                let today = new Date();
+                                date_args = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+
+                                var nonTransformedDate = parseDateAndTime(date_args, hour_min_args, msg);
+                                let d = time_utils.generateDateGivenOffset(nonTransformedDate, difference);
+
+                                if (isValidDate(d)) {
+                                    var params_stg = date_args.toString() + ' ' + hour_min_args.toString();
+                                    var now = new Date();
+                                    if (d > now) {
+                                        let ota = new cron(d, () => {
+                                            msg.channel.send(`${message}`);
+                                        });
+                                        setupCronForOTAlarm(d, msg, cron_list, now, ota, params_stg, timezone);
+                                    } else {
+                                        msg.channel.send(`The date you entered:${params_stg} already happened!`);
+                                    }
+                                } else {
+                                    msg.channel.send(`The time _${hour_min_args}_ that you have provided is invalid, it should be <Day/Month/Year>! Please correct any errors and try again!`);
+                                }
+                            }
                         }
                     } else {
-                        msg.channel.send(`The date _${date_args}_ that you have provided is invalid, it should be <Day/Month/Year>! Please correct any errors and try again!`);
-                    }
-                } else {
-                    message = args.slice(1, args.length).join(' ');
+                        let timezone = args[1];
+                        let difference = time_utils.get_offset_difference(timezone);
+                        if (args[3].includes('/') && args[3].length >= 3 && args[3].length <= 10) {
+                            date_args = args[3];
+                            message = args.slice(4, args.length).join(' ');
 
-                    let today = new Date();
-                    date_args = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
-                    var d = parseDateAndTime(date_args, hour_min_args, msg);
+                            var nonTransformedDate = parseDateAndTime(date_args, hour_min_args, msg);
+                            let d = time_utils.generateDateGivenOffset(nonTransformedDate, difference);
 
-                    if (isValidDate(d)) {
-                        var params_stg = date_args.toString() + ' ' + hour_min_args.toString();
-                        var now = new Date();
-                        if (d > now) {
-                            let ota = new cron(d, () => {
-                                msg.channel.send(`${message}`);
-                            });
-                            setupCronForOTAlarm(d, msg, cron_list, now, ota, params_stg);
+                            if (isValidDate(d)) {
+                                var params_stg = date_args.toString() + ' ' + hour_min_args.toString();
+                                var now = new Date();
+                                if (d > now) {
+                                    let ota = new cron(d, () => {
+                                        msg.author.send(`${message}`);
+                                    });
+                                    setupCronForOTAlarm(d, msg, cron_list, now, ota, params_stg, timezone);
+                                } else {
+                                    msg.channel.send(`The date you entered:${params_stg} already happened!`);
+                                }
+                            } else {
+                                msg.channel.send(`The date _${date_args}_ that you have provided is invalid, it should be <Day/Month/Year>! Please correct any errors and try again!`);
+                            }
                         } else {
-                            msg.channel.send(`The date you entered:${params_stg} already happened!`);
+                            message = args.slice(3, args.length).join(' ');
+
+                            let today = new Date();
+                            date_args = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+
+                            var nonTransformedDate = parseDateAndTime(date_args, hour_min_args, msg);
+                            let d = time_utils.generateDateGivenOffset(nonTransformedDate, difference);
+
+                            if (isValidDate(d)) {
+                                var params_stg = date_args.toString() + ' ' + hour_min_args.toString();
+                                var now = new Date();
+                                if (d > now) {
+                                    let ota = new cron(d, () => {
+                                        msg.author.send(`${message}`);
+                                    });
+                                    setupCronForOTAlarm(d, msg, cron_list, now, ota, params_stg, timezone);
+                                } else {
+                                    msg.channel.send(`The date you entered:${params_stg} already happened!`);
+                                }
+                            } else {
+                                msg.channel.send(`The time _${hour_min_args}_ that you have provided is invalid, it should be <Day/Month/Year>! Please correct any errors and try again!`);
+                            }
                         }
-                    } else {
-                        msg.channel.send(`The date _${date_args}_ that you have provided is invalid, it should be <Day/Month/Year>! Please correct any errors and try again!`);
                     }
                 }
             } else {
-                if (args[2].includes('/') && args[2].length >= 3 && args[2].length <= 10) {
-                    date_args = args[2];
-                    message = args.slice(3, args.length).join(' ');
-
-                    var d = parseDateAndTime(date_args, hour_min_args, msg);
-
-                    if (isValidDate(d)) {
-                        var params_stg = date_args.toString() + ' ' + hour_min_args.toString();
-                        var now = new Date();
-                        if (d > now) {
-                            let ota = new cron(d, () => {
-                                msg.author.send(`${message}`);
-                            });
-                            setupCronForOTAlarm(d, msg, cron_list, now, ota, params_stg);
-                        } else {
-                            msg.channel.send(`The date you entered:${params_stg} already happened!`);
-                        }
-                    } else {
-                        msg.channel.send(`The date _${date_args}_ that you have provided is invalid, it should be <Day/Month/Year>! Please correct any errors and try again!`);
-                    }
-                } else {
-                    message = args.slice(2, args.length).join(' ');
-
-                    let today = new Date();
-                    date_args = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
-                    var d = parseDateAndTime(date_args, hour_min_args, msg);
-
-                    if (isValidDate(d)) {
-                        var params_stg = date_args.toString() + ' ' + hour_min_args.toString();
-                        var now = new Date();
-                        if (d > now) {
-                            let ota = new cron(d, () => {
-                                msg.author.send(`${message}`);
-                            });
-                            setupCronForOTAlarm(d, msg, cron_list, now, ota, params_stg);
-                        } else {
-                            msg.channel.send(`The date you entered:${params_stg} already happened!`);
-                        }
-                    } else {
-                        msg.channel.send(`The date _${date_args}_ that you have provided is invalid, it should be <Day/Month/Year>! Please correct any errors and try again!`);
-                    }
-                }
+                msg.channel.send('No arguments were passed for this alarm!\n'
+                    + 'Usage: `' + auth.prefix + 'oneTimeAlarm <-p> <Timezone> <HH:MM> <Day/Month/Year> <Message>`\n'
+                    + 'Try `$help` for more information!');
             }
         } else {
             msg.channel.send('You do not have permissions to set that alarm! Ask for the admins on your server to give you the `Alarming` role!');
