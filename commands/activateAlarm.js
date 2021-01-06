7f2cd8ca88a7bebaef9513f9f7da85e7136b4557
LendaDeAfonselio <afonsof367@gmail.com>
2021-01-06 20:07:13 +0000

    activate alarm and general parameter checking

--- commands/activateAlarm.js
+++ commands/activateAlarm.js
@@ -6,43 +6,53 @@ const private_flag = auth.private_prefix;
 const temp_flag = auth.one_time_prefix;
 const logging = require('../Utils/logging');
 
-function can_delete_alarm(message, alarm_id) {
-    return (message.member.hasPermission("ADMINISTRATOR") || alarm_id.includes(message.author.id));
+function can_silence_alarm(message, alarm_id) {
+    return message.member.hasPermission("ADMINISTRATOR") || alarm_id.includes(message.author.id);
 }
 
 module.exports = {
-    name: 'deleteAlarm',
-    description: 'Deletes the alarm with a given id - **THIS ACTION CANNOT BE REVERTED**',
-    usage: auth.prefix + 'deleteAlarm <id>',
+    name: 'silenceAlarm',
+    description: 'Silences the alarm with a given id. To turn it back on use activateAlarm <id>',
+    usage: auth.prefix + 'silenceAlarm <id>',
     async execute(msg, args, client, cron, cron_list, mongoose) {
-        var alarm_to_delete = args[0];
-        if (!can_delete_alarm(msg, alarm_to_delete)) {
-            msg.channel.send(`The alarm you selected isn't yours or you aren't administrator on this server therefore you cannot delete it!`)
+        var alarm_to_silence = args[0];
+        if (!can_silence_alarm(msg, alarm_to_silence)) {
+            msg.channel.send(`The alarm you selected isn't yours or you aren't administrator on this server therefore you cannot silence it!`)
         }
         else {
-            if (cron_list[alarm_to_delete] !== undefined) {
-                try {
-                    if (alarm_to_delete.includes(private_flag)) {
-                        await Private_alarm_model.deleteOne(
-                            { alarm_id: alarm_to_delete }
-                        )
-                    } else if (!alarm_to_delete.includes(temp_flag)) {
-                        await Alarm_model.deleteOne(
-                            { alarm_id: alarm_to_delete }
-                        )
+            if (cron_list[alarm_to_silence] !== undefined) {
+                if (!cron_list[alarm_to_silence].running) {
+                    try {
+                        if (alarm_to_silence.includes(private_flag)) {
+                            await Private_alarm_model.update(
+                                { alarm_id: alarm_to_silence },
+                                {
+                                    isActive: true
+                                }
+                            )
+                        } else if (!alarm_to_silence.includes(temp_flag)) {
+                            await Alarm_model.update(
+                                { alarm_id: alarm_to_silence },
+                                {
+                                    isActive: true
+                                }
+                            )
+                        }
+                        cron_list[alarm_to_silence].start();
+                        msg.channel.send(`${alarm_to_silence} is now active again.`);
+                    } catch (e) {
+                        logging.logger.info(`Error re-activating alarm with id:${alarm_to_silence}... Please try again later!`);
+                        logging.logger.error(e);
+                        msg.channel.send(`Error re-activating alarm with id:${alarm_to_silence}... Please try again later!`);
                     }
-                    cron_list[alarm_to_delete].stop();
-                    delete cron_list[alarm_to_delete];
-                    msg.channel.send(`Sucessfully deleted alarm: ${alarm_to_delete}.`);
-                } catch (e) {
-                    logging.logger.info(`Error deleting alarm with id:${alarm_to_delete}... Please try again later!`);
-                    logging.logger.error(e);
-                    msg.channel.send(`Error deleting alarm with id:${alarm_to_delete}... Please try again later!`);
+                } else {
+                    msg.channel.send(`Alarm with id ${alarm_to_silence} has already been silenced.\nTo see your alarms, type: ${auth.prefix}${myalarms_command_stg}`);
+
                 }
             }
             else {
                 var myalarms_command_stg = "`myalarms`";
-                msg.channel.send(`Impossible to delete alarm with id ${alarm_to_delete}.\nPlease check if you entered the id of the alarm correctly!\nTo see your alarms, type: ${auth.prefix}${myalarms_command_stg}`);
+                msg.channel.send(`Impossible to silence alarm with id ${alarm_to_silence}.\nPlease check if you entered the id of the alarm correctly!\nTo see your alarms, type: ${auth.prefix}${myalarms_command_stg}`);
             }
         }
 
--- commands/alarm.js
+++ commands/alarm.js
@@ -7,64 +7,69 @@ const logging = require('../Utils/logging');
 module.exports = {
     name: 'alarm',
     description: 'Sets up an alarm that will be repeated\n' +
-        'This alarm will send a message to the _channel_ of the _server_ in which it is activated.\n'
-        + 'The parameter <target> is optional.',
-    usage: auth.prefix + 'alarm <timezone/city/UTC> <m> <h> <month> <weekday> <message> <target>',
+        'This alarm will send a message to the _channel_ of the _server_ in which it is activated.\n',
+    usage: auth.prefix + 'alarm <timezone/city/UTC> <m> <h> <month> <weekday> <message>',
     async execute(msg, args, client, cron, cron_list, mongoose) {
         if (utils.hasAlarmRole(msg, auth.alarm_role_name) || utils.isAdministrator(msg)) {
-            var timezone = args[0];
-            var crono = args.slice(1, 6).join(' ');
-            var message_stg = args.slice(6, args.length).join(' ');
-            var difference = time_utils.get_offset_difference(timezone);
-            if (difference === undefined) {
-                msg.channel.send('The timezone you have entered is invalid. Please visit https://www.timeanddate.com/time/map/ for information about your timezone!')
-            }
-            else if (time_utils.validate_alarm_parameters(msg, crono, message_stg)) {
-                crono = time_utils.updateParams(difference, crono);
-                var guild = msg.guild.id;
-                try {
-                    let scheduledMessage = new cron(crono, () => {
-                        msg.channel.send(`${message_stg}`);
-                    }, {
-                        scheduled: true
-                    });
-                    scheduledMessage.start();
-                    let alarm_user = msg.author.id;
-                    let this_alarm_id = Math.random().toString(36).substring(4);
-                    let alarm_id = `${this_alarm_id}_${alarm_user}`;
-                    // save locally
-                    cron_list[alarm_id] = scheduledMessage;
-
-                    // save to DB
-                    const newAlarm = new Alarm_model({
-                        _id: mongoose.Types.ObjectId(),
-                        alarm_id: alarm_id,
-                        alarm_args: crono,
-                        message: message_stg,
-                        guild: guild,
-                        channel: msg.channel.id,
-                        isActive: true,
-                        timestamp: Date.now(),
-                    });
-                    newAlarm.save()
-                        .then((result) => {
-                            logging.logger.info(`${result} added to database`);
-                            msg.channel.send({
-                                embed: {
-                                    fields: { name: 'Alarm added successfully!', value: `Alarm with params: ${crono} (server time) and message ${message_stg} for channel ${msg.channel.name} was added with success!` },
-                                    timestamp: new Date()
-                                }
-                            });
-                        })
-                        .catch((err) => {
-                            logging.logger.info(`An error while trying to add ${result} to the database. Message: ${newAlarm}`);
-                            logging.logger.error(err);
-                        });
-                } catch (err) {
-                    logging.logger.info(`An error while trying to add alarm with params: ${msg.content}`);
-                    logging.logger.error(err);
-                    msg.channel.send(`Error adding the alarm with params: ${crono}, with message ${message_stg}`);
+            if (args.length > 6) {
+                var timezone = args[0];
+                var crono = args.slice(1, 6).join(' ');
+                var message_stg = args.slice(6, args.length).join(' ');
+                var difference = time_utils.get_offset_difference(timezone);
+                if (difference === undefined) {
+                    msg.channel.send('The timezone you have entered is invalid. Please visit https://www.timeanddate.com/time/map/ for information about your timezone!')
                 }
+                else if (time_utils.validate_alarm_parameters(msg, crono, message_stg)) {
+                    crono = time_utils.updateParams(difference, crono);
+                    var guild = msg.guild.id;
+                    try {
+                        let scheduledMessage = new cron(crono, () => {
+                            msg.channel.send(`${message_stg}`);
+                        }, {
+                            scheduled: true
+                        });
+                        scheduledMessage.start();
+                        let alarm_user = msg.author.id;
+                        let this_alarm_id = Math.random().toString(36).substring(4);
+                        let alarm_id = `${this_alarm_id}_${alarm_user}`;
+                        // save locally
+                        cron_list[alarm_id] = scheduledMessage;
+
+                        // save to DB
+                        const newAlarm = new Alarm_model({
+                            _id: mongoose.Types.ObjectId(),
+                            alarm_id: alarm_id,
+                            alarm_args: crono,
+                            message: message_stg,
+                            guild: guild,
+                            channel: msg.channel.id,
+                            isActive: true,
+                            timestamp: Date.now(),
+                        });
+                        newAlarm.save()
+                            .then((result) => {
+                                logging.logger.info(`${result} added to database`);
+                                msg.channel.send({
+                                    embed: {
+                                        fields: { name: 'Alarm added successfully!', value: `Alarm with params: ${crono} (server time) and message ${message_stg} for channel ${msg.channel.name} was added with success!` },
+                                        timestamp: new Date()
+                                    }
+                                });
+                            })
+                            .catch((err) => {
+                                logging.logger.info(`An error while trying to add ${result} to the database. Message: ${newAlarm}`);
+                                logging.logger.error(err);
+                            });
+                    } catch (err) {
+                        logging.logger.info(`An error while trying to add alarm with params: ${msg.content}`);
+                        logging.logger.error(err);
+                        msg.channel.send(`Error adding the alarm with params: ${crono}, with message ${message_stg}`);
+                    }
+                }
+            } else {
+                msg.channel.send('Not enough parameters were passed.\n' +
+                    'Usage: ' + auth.prefix + 'alarm <timezone/city/UTC> <m> <h> <month> <weekday> <message>'
+                );
             }
         }
         else {
