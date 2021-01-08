e4b6a752d70704d9e09321e77fed1db0f2a69aa0
LendaDeAfonselio <afonsof367@gmail.com>
2021-01-08 18:50:29 +0000

    some verifications added for parameter parsing

--- commands/activateAlarm.js
+++ commands/activateAlarm.js
@@ -11,49 +11,54 @@ function can_silence_alarm(message, alarm_id) {
 }
 
 module.exports = {
-    name: 'silenceAlarm',
+    name: 'activateAlarm',
     description: 'Silences the alarm with a given id. To turn it back on use activateAlarm <id>',
     usage: auth.prefix + 'silenceAlarm <id>',
     async execute(msg, args, client, cron, cron_list, mongoose) {
-        var alarm_to_silence = args[0];
-        if (!can_silence_alarm(msg, alarm_to_silence)) {
-            msg.channel.send(`The alarm you selected isn't yours or you aren't administrator on this server therefore you cannot silence it!`)
-        }
-        else {
-            if (cron_list[alarm_to_silence] !== undefined) {
-                if (!cron_list[alarm_to_silence].running) {
-                    try {
-                        if (alarm_to_silence.includes(private_flag)) {
-                            await Private_alarm_model.update(
-                                { alarm_id: alarm_to_silence },
-                                {
-                                    isActive: true
-                                }
-                            )
-                        } else if (!alarm_to_silence.includes(temp_flag)) {
-                            await Alarm_model.update(
-                                { alarm_id: alarm_to_silence },
-                                {
-                                    isActive: true
-                                }
-                            )
-                        }
-                        cron_list[alarm_to_silence].start();
-                        msg.channel.send(`${alarm_to_silence} is now active again.`);
-                    } catch (e) {
-                        logging.logger.info(`Error re-activating alarm with id:${alarm_to_silence}... Please try again later!`);
-                        logging.logger.error(e);
-                        msg.channel.send(`Error re-activating alarm with id:${alarm_to_silence}... Please try again later!`);
-                    }
-                } else {
-                    msg.channel.send(`Alarm with id ${alarm_to_silence} has already been silenced.\nTo see your alarms, type: ${auth.prefix}${myalarms_command_stg}`);
-
-                }
+        if (args.length >= 1) {
+            var alarm_to_silence = args[0];
+            if (!can_silence_alarm(msg, alarm_to_silence)) {
+                msg.channel.send(`The alarm you selected isn't yours or you aren't administrator on this server therefore you cannot silence it!`)
             }
             else {
-                var myalarms_command_stg = "`myalarms`";
-                msg.channel.send(`Impossible to silence alarm with id ${alarm_to_silence}.\nPlease check if you entered the id of the alarm correctly!\nTo see your alarms, type: ${auth.prefix}${myalarms_command_stg}`);
+                if (cron_list[alarm_to_silence] !== undefined) {
+                    if (!cron_list[alarm_to_silence].running) {
+                        try {
+                            if (alarm_to_silence.includes(private_flag)) {
+                                await Private_alarm_model.updateOne(
+                                    { alarm_id: alarm_to_silence },
+                                    {
+                                        isActive: true
+                                    }
+                                )
+                            } else if (!alarm_to_silence.includes(temp_flag)) {
+                                await Alarm_model.updateOne(
+                                    { alarm_id: alarm_to_silence },
+                                    {
+                                        isActive: true
+                                    }
+                                )
+                            }
+                            cron_list[alarm_to_silence].start();
+                            msg.channel.send(`${alarm_to_silence} is now active again.`);
+                        } catch (e) {
+                            logging.logger.info(`Error re-activating alarm with id:${alarm_to_silence}... Please try again later!`);
+                            logging.logger.error(e);
+                            msg.channel.send(`Error re-activating alarm with id:${alarm_to_silence}... Please try again later!`);
+                        }
+                    } else {
+                        msg.channel.send(`Alarm with id ${alarm_to_silence} has already been silenced.\nTo see your alarms, type: ${auth.prefix}${myalarms_command_stg}`);
+
+                    }
+                }
+                else {
+                    var myalarms_command_stg = "`myalarms`";
+                    msg.channel.send(`Impossible to silence alarm with id ${alarm_to_silence}.\nPlease check if you entered the id of the alarm correctly!\nTo see your alarms, type: ${auth.prefix}${myalarms_command_stg}`);
+                }
             }
+        }else{
+            msg.channel.send(`No arguments were passed to execute this command.\n`
+            + 'Usage: ' + this.usage);
         }
 
     }
