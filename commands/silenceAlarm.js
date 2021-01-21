a312994233930c295c4345140c7d4f8587ca9dfd
LendaDeAfonselio <48067609+LendaDeAfonselio@users.noreply.github.com>
2021-01-21 19:25:49 +0000

    added timeout (#58)

--- commands/silenceAlarm.js
+++ commands/silenceAlarm.js
@@ -11,6 +11,45 @@ const logging = require('../Utils/logging');
 function can_silence_alarm(message, alarm_id) {
     return message.member.hasPermission("ADMINISTRATOR") || alarm_id.includes(message.author.id);
 }
+function isValidDate(d) {
+    return d instanceof Date && !isNaN(d);
+}
+
+function parseDate(date_args) {
+    let date_tokens = date_args.split('/');
+    if (date_tokens.length === 2 || date_tokens.length === 3) {
+        let day = date_tokens[0];
+        let month = date_tokens[1];
+        let year = new Date().getFullYear();
+
+        if (date_tokens.length === 3) {
+            year = date_tokens[2];
+        }
+
+        let date_stg = `${year}-${month}-${day}`;
+        let d = new Date(date_stg);
+        return d;
+    }
+    return undefined;
+}
+
+async function silenceAlarmWithID(alarm_to_silence, activate) {
+    if (alarm_to_silence.includes(private_flag)) {
+        await Private_alarm_model.updateOne(
+            { alarm_id: alarm_to_silence },
+            {
+                isActive: activate
+            }
+        );
+    } else if (!alarm_to_silence.includes(temp_flag)) {
+        await Alarm_model.updateOne(
+            { alarm_id: alarm_to_silence },
+            {
+                isActive: activate
+            }
+        );
+    }
+}
 
 module.exports = {
     name: 'silenceAlarm',
@@ -20,28 +59,43 @@ module.exports = {
         if (args.length >= 1) {
 
             var alarm_to_silence = args[0];
+
+            let timeout = undefined;
+            if (args.length >= 2) {
+                timeout = args[1];
+            }
             if (!can_silence_alarm(msg, alarm_to_silence)) {
                 msg.channel.send(`The alarm you selected isn't yours or you aren't administrator on this server therefore you cannot silence it!`)
             }
             else {
                 if (cron_list[alarm_to_silence] !== undefined) {
                     if (cron_list[alarm_to_silence].running) {
-                        try {
-                            if (alarm_to_silence.includes(private_flag)) {
-                                await Private_alarm_model.updateOne(
-                                    { alarm_id: alarm_to_silence },
-                                    {
-                                        isActive: false
-                                    }
-                                )
-                            } else if (!alarm_to_silence.includes(temp_flag)) {
-                                await Alarm_model.updateOne(
-                                    { alarm_id: alarm_to_silence },
-                                    {
-                                        isActive: false
-                                    }
-                                )
+                        if (timeout !== undefined) {
+                            let timeout_date = parseDate(timeout);
+                            if (isValidDate(timeout_date)) {
+                                let current_date = new Date();
+                                if (timeout_date > current_date) {
+                                    let c = new cron(timeout_date, async () => {
+                                        if (!cron_list[alarm_to_silence].running) {
+                                            cron_list[alarm_to_silence].start();
+                                            await silenceAlarmWithID(alarm_to_silence, true);
+                                            msg.channel.send(`Alarm with id ${alarm_to_silence} is now active again!`);
+                                        }
+                                    });
+                                    c.start();
+                                    msg.channel.send(`Alarm will be activated again in ${timeout}`);
+                                }
+                                else {
+                                    msg.channel.send('The date you have provided has already happened...');
+                                    return;
+                                }
+                            } else {
+                                msg.channel.send('The date you entered is invalid');
+                                return;
                             }
+                        }
+                        try {
+                            await silenceAlarmWithID(alarm_to_silence, false);
                             cron_list[alarm_to_silence].stop();
                             msg.channel.send(`${alarm_to_silence} is now silent.`);
                         } catch (e) {
@@ -51,7 +105,6 @@ module.exports = {
                         }
                     } else {
                         msg.channel.send(`Alarm with id ${alarm_to_silence} has already been silenced.\nTo see your alarms, type: ${auth.prefix}${myalarms_command_stg}`);
-
                     }
                 }
                 else {
@@ -61,7 +114,7 @@ module.exports = {
             }
         } else {
             msg.channel.send(`No arguments were passed to execute this command.\n`
-            + 'Usage: ' + this.usage);
+                + 'Usage: ' + this.usage);
         }
 
     }
