34d623c0317017ce49e893cd5c63f6cf21aca32f
LendaDeAfonselio <48067609+LendaDeAfonselio@users.noreply.github.com>
2021-03-07 15:50:05 +0000

    Automatic server reboot (#78)
    * added script
    
    * Update rebootBot.sh

---
+++ rebootBot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ps cax | grep node > /dev/null
+if [ $? -eq 0 ]; then
+  echo "Process is running." >/dev/null 2>&1
+else
+  echo "Process is not running."
+  PATH=$PATH:/usr/local/bin
+  pm2 start bot.js
+fi
+
+# crontab -e
+# */30 * * * * ./path/rebootBot.sh
