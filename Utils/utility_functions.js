d6b55c5b8c73ad483c5ebc3566a75d685a9815e8
LendaDeAfonselio <afonsof367@gmail.com>
2021-05-23 12:21:57 +0100

    Update utility_functions.js

--- Utils/utility_functions.js
+++ Utils/utility_functions.js
@@ -113,7 +113,7 @@ function chunkArray(myArray, chunk_size) {
     var tempArray = [];
 
     for (index = 0; index < arrayLength; index += chunk_size) {
-        myChunk = myArray.slice(index, index + chunk_size);
+        let myChunk = myArray.slice(index, index + chunk_size);
         // Do something if you want with the group
         tempArray.push(myChunk);
     }
