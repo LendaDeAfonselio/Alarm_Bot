bfc6fdec1bb5e30475d2a41ac965e0dd995a0692
LendaDeAfonselio <afonsof367@gmail.com>
2021-06-10 16:22:39 +0100

    bumping number of alarms per premium membership up and adding expirydate

--- data_access/premium_index.js
+++ data_access/premium_index.js
@@ -12,7 +12,15 @@ async function get_premium_user_by_discord_tag(disc_tag) {
     return await Premium_User_Model.findOne({ discord_user_tag: disc_tag });
 }
 
+async function delete_all_expired_memberships() {
+    let now = new Date();
+    return await Premium_User_Model.deleteMany({
+        expiry_date: { "$lt": now }
+    });
+}
+
 module.exports = {
     get_premium_user_by_id: get_premium_user_by_id,
-    get_premium_user_by_discord_tag: get_premium_user_by_discord_tag
+    get_premium_user_by_discord_tag: get_premium_user_by_discord_tag,
+    delete_all_expired_memberships: delete_all_expired_memberships
 }
