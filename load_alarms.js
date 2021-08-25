1dc02dd224d84313d44c7baaf59c77b1719aedfe
LendaDeAfonselio <afonsof367@gmail.com>
2021-08-25 20:22:11 +0100

    appears to be working fine

--- load_alarms.js
+++ load_alarms.js
@@ -3,7 +3,6 @@ const Private_alarm_model = require('./models/private_alarm_model');
 const One_Time_Alarm_model = require('./models/one_time_alarm_model');
 const alarm_db = require('./data_access/alarm_index');
 const logging = require('./Utils/logging');
-const utility_functions = require('./Utils/utility_functions');
 
 async function fetchAlarmsforGuild(cron_list, cron, guild_id, client) {
     let alarms = await Alarm_model.find({ guild: guild_id });
@@ -50,15 +49,16 @@ async function fetchPrivateAlarms(cron_list, cron, client) {
         let user_id = alarm.user_id;
         let scheduledMessage = new cron(crono, async () => {
             try {
-                let member = await client.users.fetch(user_id);
-                if (member !== undefined) {
-                    member.send(message_stg);
-                } else {
-                    logging.logger.info(`${alarm_id} from the DB is not usable because the user was not found`);
-                    await alarm_db.delete_private_alarm_with_id(alarm_id);
-                }
+                client.shard.broadcastEval(`
+                (async () => {
+                    let member = await this.users.fetch("${user_id}");
+                    if (member !== undefined) {
+                        member.send("${message_stg}");
+                    } 
+                })()`);
             } catch (err) {
                 logging.logger.error(`Alarm with id ${alarm_id} failed to go off. Error: ${err}`);
+                await alarm_db.delete_private_alarm_with_id(alarm_id);
             }
         }, {
             scheduled: true
@@ -116,7 +116,6 @@ async function fetchPrivateOTAs(cron_list, cron, client) {
     let current = new Date();
     let alarms = await One_Time_Alarm_model.find({ isPrivate: true });
     for (alarm of alarms) {
-        console.log("rei ricardo");
 
         let alarm_id = alarm.alarm_id;
         let crono = alarm.alarm_date;
@@ -128,17 +127,21 @@ async function fetchPrivateOTAs(cron_list, cron, client) {
         let user_id = alarm.user_id;
 
         let scheduledMessage = new cron(crono, async () => {
-            await client.shard.broadcastEval(`
+            try {
+
+                await client.shard.broadcastEval(`
             (async () => {
                 let member = await this.users.fetch("${user_id}");
                 if (member !== undefined) {
                     member.send("${message_stg}");
-                } else {
-                    logging.logger.info(\`${alarm_id} from the DB is not usable because the user was not found\`);
                 }
             })()`);
-            scheduledMessage.stop();
-            delete cron_list[alarm_id];
+                scheduledMessage.stop();
+                delete cron_list[alarm_id];
+            } catch (err) {
+                logging.logger.info(`${alarm_id} from the DB is not usable because the user was not found`);
+                await alarm_db.delete_oneTimeAlarm_with_id(alarm_id);
+            }
         });
 
         scheduledMessage.start();
