1bc2cc072831094515a8e1822eb6c66ecb662170
Afonso Falcão <48067609+LendaDeAfonselio@users.noreply.github.com>
2022-06-08 11:04:17 +0100

    Add reconnect to shards (#123)

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const { ShardingManager } = require('discord.js');
 const appsettings = require('./appsettings.json');
+const logging = require('./Utils/logging');
 
 const shards = new ShardingManager('./bot.js', {
     totalShards: 'auto', // this value should always be 'auto', if its a number it should only be used for testing
     token: appsettings.token,
     execArgv: ['--trace-warnings'],
+    respawn: true
 });
 
 shards.on('shardCreate', async (shard) => {
     shard.on("ready", () => {
-        console.log(`New shard with id ${shard.id}`);
+        logging.logger.info(`New shard with id ${shard.id}`);
 
         shard.send({ type: "shardId", data: { shardId: shard.id } });
     });
+    shard.on('reconnecting', (a, b) => {
+        logging.logger.info(`Shard ${shard.id} reconnecting`);
+        logging.logger.info(a);
+        logging.logger.info(b);
+        shard.send({ type: "shardId", data: { shardId: shard.id } });
+
+    })
 });
 
 shards.spawn().catch(error => console.error(`[ERROR/SHARD] Shard failed to spawn.\n${error}`));
